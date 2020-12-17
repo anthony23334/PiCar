2f84b7cbb5905828cc28ac5137fee8d7ed4220a1
Anthony Ngoma <an474@cornell.edu>
2020-12-16 19:24:49 -0600

    more changes to the script

--- project/website/client.sh
+++ project/website/client.sh
@@ -2,6 +2,10 @@
 WEB_PORT=5000
 CAM_PORT=8000
 CLASS_IP=132.236.79.205
+
+WEB_PORT_CLASS=0
+CAM_PORT_CLASS=0
+
 # Make sure that you kill any prev camera instances running! 
 kill $(pgrep -f 'python3') > /dev/null 
 
@@ -9,18 +13,28 @@ echo '====== Welcome to PiCar (Client) ======'
 echo 'Please enter your netid: '
 read netid 
 
+if [[ "$netid" == "akk72" ]] ; then 
+  WEB_PORT_CLASS = 8888
+  CAM_PORT_CLASS = 9999
+fi 
+
+if [[ "$netid" == "an474" ]] ; then 
+  WEB_PORT_CLASS = 2222
+  CAM_PORT_CLASS = 1111
+fi 
+
 if [[ $1 -eq 0 ]] ; then 
   # Open the camera tunnel (RECV):
-  echo '====== Ssh-tunneling camera (9999) ====== '
-  ssh -fNL $CAM_PORT:localhost:9999 $netid@$CLASS_IP 
+  echo '====== Ssh-tunneling camera ====== '
+  ssh -fNL $CAM_PORT:localhost:$WEB_PORT_CLASS $netid@$CLASS_IP 
   echo "*********done*********"
 
   echo ""
   echo ""
 
   # Open the website tunnel(RECV):
-  echo '====== Ssh-tunneling server (8888) ======'
-  ssh -fNL $WEB_PORT:localhost:8888 $netid@$CLASS_IP 
+  echo '====== Ssh-tunneling server ======'
+  ssh -fNL $WEB_PORT:localhost:$CAM_PORT_CLASS $netid@$CLASS_IP 
   echo '*********done*********'
 
   echo ""
--- project/website/run.sh
+++ project/website/run.sh
@@ -2,6 +2,10 @@
 WEB_PORT=5000
 CAM_PORT=8000
 CLASS_IP=132.236.79.205
+
+WEB_PORT_CLASS=0
+CAM_PORT_CLASS=0
+
 # Make sure that you kill any prev camera instances running! 
 kill $(pgrep -f 'python3')
 
@@ -9,17 +13,27 @@ echo '====== Welcome to PiCar (Server) ======'
 echo 'Please enter your netid: '
 read netid 
 
+if [[ "$netid" == "akk72" ]] ; then 
+  WEB_PORT_CLASS = 8888
+  CAM_PORT_CLASS = 9999
+fi 
+
+if [[ "$netid" == "an474" ]] ; then 
+  WEB_PORT_CLASS = 2222
+  CAM_PORT_CLASS = 1111
+fi 
+
 if [[ $1 -eq 0 ]] ; then 
   # Open the camera tunnel (RECV):
-  echo '====== Ssh-tunneling camera (9999) ====== '
-  ssh -fNR 9999:localhost:$CAM_PORT $netid@$CLASS_IP > /dev/null
+  echo '====== Ssh-tunneling camera ====== '
+  ssh -fNR CAM_PORT_CLASS:localhost:$CAM_PORT $netid@$CLASS_IP > /dev/null
   echo "*********done*********"
 
   echo ""
   echo ""
 
   # Start the camera server and put in the background 
-  echo '====== Starting the camera server (8000) ======'
+  echo '====== Starting the camera server ======'
   python3 ../camera/cam.py & > /dev/null
   echo '*********done*********'
 
@@ -27,8 +41,8 @@ if [[ $1 -eq 0 ]] ; then
   echo ""
 
   # Open the website tunnel(RECV):
-  echo '====== Ssh-tunneling server (8888) ======'
-  ssh -fNR 8888:localhost:$WEB_PORT $netid@$CLASS_IP > /dev/null 
+  echo '====== Ssh-tunneling server ======'
+  ssh -fNR WEB_PORT_CLASS:localhost:$WEB_PORT $netid@$CLASS_IP > /dev/null 
   echo '*********done*********'
 
   echo ""
@@ -36,7 +50,7 @@ if [[ $1 -eq 0 ]] ; then
 fi 
 if [[ $1 -eq 1 ]] ; then 
   # Start the camera server and put in the background 
-  echo '====== Starting the camera server (8000) ======'
+  echo '====== Starting the camera server ======'
   #python3 ../camera/cam.py & > /dev/null
   echo '*********done*********'
 fi 
